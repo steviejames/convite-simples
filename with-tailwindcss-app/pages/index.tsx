30fd52de266361b3c433c5d1f4114040450f7963
iverson da Piedade <iversoncosta18@gmail.com>
2022-06-11 04:32:48 +0100

    Adição de informações de ajuda

--- with-tailwindcss-app/pages/index.tsx
+++ with-tailwindcss-app/pages/index.tsx
@@ -20,22 +20,27 @@ const Home: NextPage = () => {
         <h1 className="text-18.75x font-mono px-20">
           VERIFICADOR DE TRANSFERÊNCIAS/DEPÓSITOS
         </h1>
+        
+        <h2 className='inline-flex pt-3 space-x-2'><div>Verifique se um comprovativo é</div><div className='bg-green-100'>verdadeiro</div><div>ou</div><div className='bg-red-100 '>falso.</div></h2>
+        
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 bg-orange-100 rounded-tl-xl rounded-tr-xl space-around sm:w-full">
           
-          <div className='flex flex-col p-6 text-left'>
+          <div className='flex flex-col p-6 pt-4 pb-0 text-left'>
             <label className='pb-2'>Número do comprovativo:</label>
             <input className='p-3 w-96' placeholder='A0000001100100'></input>
           </div>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
+          <div className="inline-flex p-6 pt-0 pl-0 mt-0 text-left w-96 rounded-xl hover:text-blue-600 focus:text-blue-600">
+          <Image 
+          src="/tapago.svg" 
+          alt="Informação" 
+          width={150} 
+          height={150} 
+          />
+            <p className="mt-4 text-sm">
+             Insira no campo acima, o número mostrado no comprovativo
             </p>
-          </a>
+          </div>
 
           <a
             href="https://github.com/vercel/next.js/tree/canary/examples"
